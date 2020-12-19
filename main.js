f74c9e65a4eebc62b6bef2fc6fbdac1e5b36eae4
Tung Nghia Nguyen <73808918+s3879966@users.noreply.github.com>
2020-12-19 18:12:25 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+alert("Welcome to my Assignment 2 website")
+function openForm() {
+    document.getElementById("livechatbox").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("livechatbox").style.display = "none";
+  }
